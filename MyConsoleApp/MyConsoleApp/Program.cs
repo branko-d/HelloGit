9cec892cc4aa2aa55c477456a5c504cc3d80d153
branko-d <branko-d@users.noreply.github.com>
2015-11-08 15:39:56 +0100

    hello

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace MyConsoleApp {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hello Git!");
         }
     }
 }
